e8b6a76b89bdda85478f9c1fe5885d8623dfc1e3
chintan5588@gmail.com <chintan5588@gmail.com>
2021-03-03 22:05:28 -0600

    added entities

---
+++ Entities/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityServer.Entities
+{
+    public class AppUser
+    {
+        public DateTime DateOfBirth { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public ICollection<Course> Courses { get; set; }
+        public ICollection<AppUserRole> UserRoles { get; set; }
+
+    }
+}
